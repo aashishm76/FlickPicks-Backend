ffabd6b829af8c6ecb258aa628b8033ca8c974d3
Aashish <36056138+aashishm76@users.noreply.github.com>
2020-11-22 19:08:36 -0500

    Fixed port used for server

--- startup/config.js
+++ startup/config.js
@@ -2,6 +2,6 @@ const config = require("config");
 
 module.exports = function (app) {
   app.listen(config.get("PORT"), () => {
-    console.log("Listening on port 8080");
+    console.log("Listening on port 5000");
   });
 };
